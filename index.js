5c19cece1233f30250cfba8f02dc507d1daed95b
Emanuel Gaona <gaonaemanuel7@gmail.com>
2023-06-21 06:41:49 -0700

    db.json modified

--- index.js
+++ index.js
@@ -1,61 +1,16 @@
-// Chessboard representation
-let board = [
-    ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
-    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-    // ... Fill other rows with pieces
-    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
-  ];
-  
-  // Generate the chessboard HTML
-  function generateBoard() {
-    const boardElement = document.querySelector('.board');
-    for (let row = 0; row < 8; row++) {
-      for (let col = 0; col < 8; col++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.id = String.fromCharCode(97 + col) + (8 - row);
-        const piece = board[row][col];
-        if (piece !== '') {
-          const pieceElement = document.createElement('span');
-          pieceElement.classList.add('piece');
-          pieceElement.textContent = piece;
-          square.appendChild(pieceElement);
-        }
-        boardElement.appendChild(square);
-      }
-    }
-  }
-  
-  // Add event listener for square clicks
-  function addSquareClickListeners() {
-    const squares = document.querySelectorAll('.square');
-    squares.forEach(square => {
-      square.addEventListener('click', handleSquareClick);
-    });
-  }
-  
-  // Handle square click event
-  function handleSquareClick(event) {
-    const square = event.target;
-    const squareId = square.id;
-    // Handle the logic for moving the piece and updating the board
-  
-    // Refresh the board display
-    clearBoard();
-    generateBoard();
-    addSquareClickListeners();
-  }
-  
-  // Clear the board display
-  function clearBoard() {
-    const boardElement = document.querySelector('.board');
-    while (boardElement.firstChild) {
-      boardElement.firstChild.remove();
-    }
-  }
-  
-  // Generate and initialize the chessboard
-  generateBoard();
-  addSquareClickListeners();
-  
+let addCars = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const addBtn= document.querySelector("#new-car-btn");
+    const carFormContainer = document.querySelector(".container");
+    console.log(carFormContainer)
+
+    addBtn.addEventListener("click", (e) => {
+            addCar= !addCar;
+            if(addCar){
+                carFormContainer.style.display = "block";
+            }else {
+                carFormContainer.style.display = "none";
+            }
+        });
+})
