34b4a7b972cbf6557ee2bd0fb811b6b759b90176
Emanuel Gaona <gaonaemanuel7@gmail.com>
2023-06-21 09:17:16 -0700

    forEach loop in js code

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
+const API = "http://localhost:3000/cars"
+
+
 let addCars = false;
 
 document.addEventListener("DOMContentLoaded", () => {
     const addBtn= document.querySelector("#new-car-btn");
     const carFormContainer = document.querySelector(".container");
-    console.log(carFormContainer)
 
-    addBtn.addEventListener("click", (e) => {
-            addCar= !addCar;
-            if(addCar){
+    fetch(API)
+      .then((response) => response.json())
+      .then(renderCars)
+
+      function renderCars(cars) {
+        console.log(cars);
+
+
+
+        CarsList.forEach((cars) => {
+            const card = document.createElement("div");
+            card.classList("card")
+
+            const img = document.createElement("img");
+            img.classList("card-img-top");
+            img.src = cars.imageUrl;
+        })
+
+
+
+      }
+
+
+
+    
+
+
+
+
+    document.addEventListener("click", () => {
+        console.log("clicking button")
+
+            addCars = !addCars;
+            if(addCars){
                 carFormContainer.style.display = "block";
             }else {
                 carFormContainer.style.display = "none";
